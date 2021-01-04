bd663f13653a50a5d04927f8bb40e2ec9faa212b
njayman <najishm828282@gmail.com>
2021-01-04 09:25:54 +0600

    Initialized with basic chat

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changes
+
+## Added:
+
+- Chat interface
--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Paper, Grid, TextField, Button } from "@material-ui/core";
+import { SendRounded } from "@material-ui/icons";
+import io from "socket.io-client";
+import "./App.css";
+
+const socket = io.connect("http://localhost:5001");
 
 function App() {
+  const [name] = useState("najish");
+  const [chat, setChat] = useState([]);
+  const [message, setMessage] = useState("");
+  const renderChat = () => {
+    return chat.map(({ name, message }, index) => (
+      <div key={index}>
+        <h3>
+          {name}: {message}
+        </h3>
+      </div>
+    ));
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("message", { name, message });
+  };
+  useEffect(() => {
+    socket.on("message", ({ name, message }) => {
+      setChat([...chat, { name, message }]);
+    });
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid
+      container
+      direction="column"
+      justify="center"
+      alignItems="center"
+      style={{ height: "100vh" }}
+    >
+      <Paper elevation={3} style={{ padding: "20px", margin: "10px" }}>
+        {renderChat()}
+      </Paper>
+      <Paper elevation={3} style={{ padding: "20px", margin: "10px" }}>
+        <form onSubmit={handleSubmit} autoComplete="off">
+          <TextField
+            multiline
+            rowsMax={4}
+            varient="filled"
+            label="Lets chat - type to chat"
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button variant="contained" color="primary" type="submit">
+            <SendRounded />
+          </Button>
+        </form>
+      </Paper>
+    </Grid>
   );
 }
 
